187669b8d14b6728fe9aa1a2f0bbc1c0df39314f
Alexxis Johnson <alexxis.johnson@capitalone.com>
2017-12-17 14:12:35 -0800

    Get initial app working

--- src/components/results_list/results.component.js
+++ src/components/results_list/results.component.js
@@ -8,12 +8,22 @@ import './Results.component.css';
 class Results extends Component {
   constructor(props) {
     super(props);
+    this.getListElements = this.getListElements.bind(this);
+  }
+
+  getListElements() {
+    return this.props.listResults.map( (restaurant) =>
+      <div>
+        {restaurant.name}
+      </div>
+    );
   }
 
   render() {
     return (
       <div className="results">
         <h3>results found</h3>
+        {this.getListElements()}
         <button>Show More</button>
       </div>
     );
--- src/components/search_bar/search.component.js
+++ src/components/search_bar/search.component.js
@@ -2,13 +2,44 @@ import React, { Component } from 'react';
 import Sidebar from '../sidebar_filters_and_ranks/Sidebar.component';
 import Results from '../results_list/Results.component';
 import './Search.component.css';
+
+
 /**
   * The Search component implements the Algolia search API to allow the user to find restaurants
   */
-
 class Search extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      listResults: [],
+      infoResults: [],
+      searchValue: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.listenForResults();
+  }
+
+  handleChange(event) {
+    let listHelper = this.props.listHelper;
+    let infoHeper = this.props.infoHeper;
+    this.setState({ searchValue: event.target.value }, function() {
+      listHelper.setQuery(this.state.searchValue).search();
+    });
+  }
+
+  listenForResults() {
+    let context = this;
+    this.props.listHelper.on('result', (restaurantList) => {
+      console.log('restaurantList', restaurantList)
+      context.setState({ listResults: restaurantList.hits }, function() {
+        console.log('list results ', this.state.listResults)
+      });
+    });
+    this.props.infoHelper.on('result', (restaurantInfo) => {
+      context.setState({ infoResults: restaurantInfo }, function() {
+        console.log('restaurantInfo ', restaurantInfo)
+      });
+    });
   }
 
   render() {
@@ -18,11 +49,18 @@ class Search extends Component {
           <input
             className="search-input"
             type="text"
-            placeholder="Search for Restaurants by Name, Cuisine, Location">
+            placeholder="Search for Restaurants by Name, Cuisine, Location"
+            value={this.state.searchValue}
+            onChange={this.handleChange}>
           </input>
         </div>
         <div className="sidebar-placement"><Sidebar /></div>
-        <div className="results-placement"><Results /></div>
+        <div className="results-placement">
+          <Results
+            listResults={this.state.listResults}
+            infoResults={this.state.infoResults}
+          />
+        </div>
       </div>
     );
   }
